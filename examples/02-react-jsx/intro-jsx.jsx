7d39d399e9d8a432d3a14ba9c2eb1007b5c74db7
Sam Genduso <sam.genduso@gmail.com>
2015-11-27 17:14:19 -0500

    intro to JSX with examples

---
+++ examples/02-react-jsx/intro-jsx.jsx
@@ -0,0 +1,16 @@
+var element = <p className="bold">Tokyo Dog</p>;
+
+var element = <ul>
+  <li className="completed">Molly Moon</li>
+  <li>Pie Mobile</li>
+</ul>;
+
+var Truck = React.createClass({
+  render: function() {
+    return <div>
+      <a href='http://www.streetdonuts.com/'>Street Donuts</a>
+    </div>;
+  }
+})
+
+var element = <Truck />;
