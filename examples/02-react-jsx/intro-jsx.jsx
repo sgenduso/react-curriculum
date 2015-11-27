cdca2e35020de71dc45ffda51783c066afb0e6ae
Sam Genduso <sam.genduso@gmail.com>
2015-11-27 17:24:02 -0500

    attribute expressions

--- examples/02-react-jsx/intro-jsx.jsx
+++ examples/02-react-jsx/intro-jsx.jsx
@@ -7,10 +7,30 @@ var element = <ul>
 
 var Truck = React.createClass({
   render: function() {
-    return <div>
+    return <li>
       <a href='http://www.streetdonuts.com/'>Street Donuts</a>
-    </div>;
+    </li>;
   }
 })
 
-var element = <Truck />;
+var Trucks = React.createClass({
+  render: function() {
+    return <ul>
+      <Truck />
+    </ul>;
+  }
+})
+
+var element = <Trucks />;
+
+var source = 'http://i.imgur.com/nVPXKtz.jpg';
+var element = <img src={source} alt = 'Saffron Spice' />;
+
+var element = <input
+  onChange={this.handleChange}
+  type='text'
+  value={this.state.searchTerm}
+/>;
+
+var food = 'Cheese';
+var element = <p>{food} Wizards</p>;
