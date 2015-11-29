916d1f8d3439d8c97071d1a6762f458dd24b592e
Sam Genduso <sam.genduso@gmail.com>
2015-11-28 19:18:44 -0500

    boolean attributes and explicit comments

--- examples/02-react-jsx/intro-jsx.jsx
+++ examples/02-react-jsx/intro-jsx.jsx
@@ -1,3 +1,4 @@
+//Nested HTML tags
 var element = <p className="bold">Tokyo Dog</p>;
 
 var element = <ul>
@@ -5,6 +6,7 @@ var element = <ul>
   <li>Pie Mobile</li>
 </ul>;
 
+//Nested React component class
 var Truck = React.createClass({
   render: function() {
     return <li>
@@ -23,14 +25,22 @@ var Trucks = React.createClass({
 
 var element = <Trucks />;
 
+//Single-line attribute expressions
 var source = 'http://i.imgur.com/nVPXKtz.jpg';
 var element = <img src={source} alt = 'Saffron Spice' />;
 
+//Multi-line attribute expressions
 var element = <input
   onChange={this.handleChange}
   type='text'
   value={this.state.searchTerm}
 />;
 
+//Child expressions
 var food = 'Cheese';
 var element = <p>{food} Wizards</p>;
+
+//Boolean attributes
+var element1 = <input type='button' disabled />;
+var element2 = <input type='button' disabled={true} />;
+var element3 = <input type='button' disabled={false} />;
